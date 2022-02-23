741dc7501600688dc6f0da9d30ffcd96930ef2eb
kirillkychkin <kychkin.kirill@gmail.com>
2022-02-23 16:51:05 +0900

    initial commit

---
+++ practice-19-5.cpp
@@ -0,0 +1,16 @@
+// practice-19-5.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+class Animal
+{
+public:
+    void Voice()
+    {
+        std::cout << "Animal voice!" << "\n";
+    }
+};
+int main()
+{
+    std::cout << "Hello World!\n";
+}
